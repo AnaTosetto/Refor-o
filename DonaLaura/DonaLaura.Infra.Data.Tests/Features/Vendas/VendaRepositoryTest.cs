bdeacaa66c7e2b40fb9000d4858f4bbf51df158f
AnaTosetto <ana_tosetto@hotmail.com>
2018-05-13 22:13:24 -0300

    Iniciando os testes de VendaRepository

---
+++ DonaLaura/DonaLaura.Infra.Data.Tests/Features/Vendas/VendaRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using DonaLaura.Common.Tests.Base;
+using DonaLaura.Common.Tests.Features;
+using DonaLaura.Domain.Features.Vendas;
+using DonaLaura.Dominio.Features.Produtos;
+using DonaLaura.Infra.Data.Features.Vendas;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonaLaura.Infra.Data.Tests.Features.Vendas
+{
+    public class VendaRepositoryTest
+    {
+        VendaRepository _vendaRepository;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            VendaBaseSqlTest.SeedDatabase();
+            _vendaRepository = new VendaRepository();
+        }
+
+        [Test]
+        public void VendaRepository_Adicionar_DeveRetornarOk()
+        {
+            //Arrange
+            Produto produto = new Produto();
+            long idProduto = 1;
+            produto.Id = idProduto;
+            long id = 0;
+            Venda venda = ObjectMother.getValidoVenda();
+            venda.Id = id;
+            venda.Produto = produto;
+
+            //Action
+            venda = _vendaRepository.Adicionar(venda);
+
+            //Verify
+            venda.Id.Should().BeGreaterThan(0);
+        }
+    }
+}
--- DonaLaura/DonaLaura.Infra.Data/Features/Vendas/VendaRepository.cs
+++ DonaLaura/DonaLaura.Infra.Data/Features/Vendas/VendaRepository.cs
@@ -1,12 +1,92 @@
-﻿using System;
+﻿using DonaLaura.Domain.Features.Vendas;
+using DonaLaura.Dominio.Features.Produtos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DonaLaura.Infra.Data.Features.Vendas
 {
-    class VendaRepository
+    public class VendaRepository : IVendaRepository
     {
+        string _sqlInsert = @"INSERT INTO Venda 
+                                        (NomeCliente, 
+                                        Quantidade, 
+                                        Lucro, 
+                                        ProdutoId)
+                                    VALUES
+                                        (@NomeCliente, 
+                                         @Quantidade, 
+                                         @Lucro, 
+                                         @ProdutoId);";
+
+        string _sqlUpdate = @"UPDATE Venda
+                                SET NomeCliente = @NomeCliente, 
+                                        Quantidade = @Quantidade, 
+                                        Lucro = @Lucro, 
+                                        ProdutoId = @ProdutoId
+                                    WHERE Id = @Id";
+
+        string _sqlDelete = @"DELETE FROM Venda
+                                WHERE Id = @Id";
+
+        string _sqlGet = @"SELECT * FROM Venda
+                            WHERE Id = @Id";
+
+        string _sqlGetAll = @"SELECT * FROM Venda";
+                                    
+
+        public Venda Adicionar(Venda venda)
+        {
+            venda.Id = Db.Insert(_sqlInsert, Take(venda));
+            return venda;
+        }
+
+        public Venda Atualizar(Venda entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Venda entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Venda Obter(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Venda> ObterTudo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private object[] Take(Venda venda)
+        {
+            return new object[]
+            {
+                "@Id", venda.Id,
+                "@NomeCliente", venda.NomeCliente,
+                "@Quantidade", venda.Quantidade,
+                "@Lucro", venda.Lucro,
+                "@ProdutoId", venda.Produto
+            };
+        }
+
+        private static Func<IDataReader, Venda> Make = reader =>
+        new Venda
+        {
+            Id = Convert.ToInt64(reader["Id"]),
+            NomeCliente = reader["NomeCliente"].ToString(),
+            Quantidade = Convert.ToInt32(reader["Quantidade"]),
+            Lucro = Convert.ToDouble(reader["Lucro"]),
+            Produto = new Produto
+            {
+                Id = Convert.ToInt64(reader["ProdutoId"]),
+            }
+        };
     }
 }
