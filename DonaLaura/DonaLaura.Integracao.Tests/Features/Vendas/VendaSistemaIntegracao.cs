b975a8bc4da661f7683c34065200fce1bf987619
AnaTosetto <ana_tosetto@hotmail.com>
2018-05-14 11:53:35 -0300

    Criado os testes de integracao para Venda, falta corrigir erros

--- DonaLaura/DonaLaura.Integracao.Tests/Features/Vendas/VendaSistemaIntegracao.cs
+++ DonaLaura/DonaLaura.Integracao.Tests/Features/Vendas/VendaSistemaIntegracao.cs
@@ -49,7 +49,7 @@ namespace DonaLaura.Integracao.Tests.Features.Vendas
             vendaResultado.NomeCliente.Should().Be(venda.NomeCliente);
             vendaResultado.Quantidade.Should().Be(venda.Quantidade);
             vendaResultado.Lucro.Should().Be(venda.Lucro);
-            vendaResultado.Produto.Should().Be(venda.Produto);
+            vendaResultado.Produto.Id.Should().Be(venda.Produto.Id);
 
             Venda vendaGet = _vendaService.Obtem(vendaResultado.Id);
 
@@ -97,6 +97,126 @@ namespace DonaLaura.Integracao.Tests.Features.Vendas
 
             //Verify
             acaoResultado.Should().Throw<QuantidadeMenorOuIgualZeroException>();
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_Atualizar_DeveRetornarOk()
+        {
+            //Arrange
+            Venda vendaParaEditar = _vendaService.Obtem(1);
+            string vendaVelha = vendaParaEditar.NomeCliente;
+            string novaVenda = "Venda";
+
+            if (vendaVelha == novaVenda)
+            {
+                novaVenda = "Nova venda";
+            }
+
+            vendaParaEditar.NomeCliente = novaVenda;
+
+            //Action
+            Venda vendaResultado = _vendaService.Atualiza(vendaParaEditar);
+
+            //Verify
+            vendaResultado.NomeCliente.Should().NotBe(vendaVelha);
+            vendaResultado.Id.Should().Be(vendaParaEditar.Id);
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_Atualizar_DeveRetornarExcecao()
+        {
+            //Arrange
+            Produto produto = new Produto();
+            long idProduto = 1;
+            produto.Id = idProduto;
+            Venda venda = ObjectMother.getValidoVenda();
+            venda.Id = 0;
+            venda.Produto = produto;
+
+            //Action
+            Action acaoResultado = () => _vendaService.Atualiza(venda);
+
+            //Verify
+            acaoResultado.Should().Throw<IdentificadorIndefinidoException>();
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_Excluir_DeveRetornarOk()
+        {
+            //Arrange
+            Produto produto = new Produto();
+            long idProduto = 1;
+            produto.Id = idProduto;
+            Venda venda = ObjectMother.getValidoVenda();
+            venda.Id = 0;
+            venda.Produto = produto;
+            
+
+            venda = _vendaService.Adiciona(venda);
+
+            //Action
+            _vendaService.Exclui(venda);
+
+            //Verify
+            Venda vendaInexistente = _vendaService.Obtem(venda.Id);
+            vendaInexistente.Should().BeNull();
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_Excluir_DeveRetornarExcecao()
+        {
+            //Arrange
+            Produto produto = new Produto();
+            long idProduto = 1;
+            produto.Id = idProduto;
+            Venda venda = ObjectMother.getValidoVenda();
+            venda.Id = 0;
+            venda.Produto = produto;
+
+            //Action
+            Action acaoResultado = () => _vendaService.Exclui(venda);
+
+            //Verify
+            acaoResultado.Should().Throw<IdentificadorIndefinidoException>();
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_Obter_DeveRetornarOk()
+        {
+            //Arrange
+            Venda venda = new Venda();
+            venda.Id = 1;
+
+            //Action
+            venda = _vendaService.Obtem(venda.Id);
+
+            //Verify
+            venda.Id.Should().Be(1);
+            venda.Should().NotBeNull();
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_Obter_DeveRetornarExcecao()
+        {
+            //Action
+            Venda vendaResultado = _vendaService.Obtem(9999999999999);
+
+            //Verify
+            vendaResultado.Should().BeNull();
+        }
+
+        [Test]
+        public void VendaSistemaIntegracao_ObterTudo_DeveRetornarOk()
+        {
+            //Arrange
+            IEnumerable<Venda> listaVenda;
+
+            //Action
+            listaVenda = _vendaService.ObtemTudo();
+
+            //Verify
+            listaVenda.Should().NotBeNull();
+            listaVenda.Count().Should().BeGreaterOrEqualTo(0);
         }
 
     }
